d4fbec959cc1d75ab4ec02ee3b9a78b8f1086b41
edmundo papi <edmundopierre@yahoo.com>
2020-04-10 03:52:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,6 +7,13 @@ attr_accessor :name
   def add_post(post)
     post.author = self
   end
+ def posts
 
+ end
+ def add_post_by_title(post_title)
 
+   new_post = self.new(post_tile)
+   add_post(new_post)
+
+ end
 end
