4c259abc06d619253daf26615947dcca7ad699db
edmundo papi <edmundopierre@yahoo.com>
2020-04-10 03:45:41 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,25 @@
 class Post
-attr_accessor :title
+attr_accessor :title, :author
 
 @@all= []
 def initialize(title)
   @title=title
   @@all << self
+end
+
+def self.all
+  @@all
+
+end
+def author_name(name)
+
+  @@all.select do |name|
+    if name.author == self
+      return name.author
+    else
+      nil
+    end
+end
 
 end
 end
