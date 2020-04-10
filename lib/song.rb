152175228d4b0d665e01ff02c57028dd0fd18f11
edmundo papi <edmundopierre@yahoo.com>
2020-04-10 03:16:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :artist, :genre, :name
+ @@all = []
+ def initialize(name)
+
+   @name = name
+   @genre = genre
+  # @artist = artist
+   save
+ end
+
+ def save
+   @@all  << self
+ end
+
+ def self.all
+   @@all
+ end
+
+end
